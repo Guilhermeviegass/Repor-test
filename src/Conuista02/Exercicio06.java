ce91b689c19b503a1214c0c44d74a4f0aa4ec4f5
Guilherme <guiguiviegas@hotmail.com>
2022-05-16 18:09:47 -0300

    mensagem

---
+++ src/Conuista02/Exercicio06.java
@@ -0,0 +1,28 @@
+package Conuista02;
+
+public class Exercicio06{
+    public static void main(String[] args) {
+
+
+        String car = "Jaguar";
+
+        switch (car) {
+
+            case "Mercedes":
+                System.out.println("Mercedes");
+                break;
+
+            case "Tesla":
+                System.out.println("Tesla");
+                break;
+
+            case "Ferrari":
+                System.out.println("Ferrari");
+                break;
+
+            default:
+                System.out.println("O valor não é compatível");
+        }
+
+    }
+}
